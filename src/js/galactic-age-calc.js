a69756c70d7362ffc61be9039eaff678af47058a
Jason Falk <jasonfalk9@gmail.com>
2023-08-09 10:31:52 -0700

    Add code for ghostOfAgesPast elapsedPastEarthTime.

--- src/js/galactic-age-calc.js
+++ src/js/galactic-age-calc.js
@@ -7,10 +7,14 @@ export default class GalacticAgeCalc {
     this.venusTime = this.presentAge/.615;
     this.marsTime = this.presentAge/1.88;
     this.jupiterTime = this.presentAge/11.86;
-    this.elapsedEarthTime = null;
-    this.elapsedMercuryTime = null;
-    this.elapsedVenusTime = null;
-    this.elapsedMarsTime = null;
-    this.elapsedJupiterTime = null;
+    this.elapsedPastEarthTime = null;
+    this.elapsedPastMercuryTime = null;
+    this.elapsedPastVenusTime = null;
+    this.elapsedPastMarsTime = null;
+    this.elapsedPastJupiterTime = null;
+  }
+
+  ghostOfAgesPast() {
+    this.elapsedPastEarthTime = this.presentAge - this.pastAge;
   }
 }
